07b2ea815f2f999c0227e0fe71b8ab29cf6172ee
EnzoGiovanni <EnzoGiovanni@users.noreply.github.com>
2017-03-23 12:42:15 +0100

    comments year, month and week selection

--- Defects/Defects Extraction.sql
+++ Defects/Defects Extraction.sql
@@ -3,15 +3,15 @@ SELECT
 BG_BUG_ID As "id",
 ------------------------------------------------------------
 to_char(BG_DETECTION_DATE, 'iyyy')||'-'||to_char(BG_DETECTION_DATE, 'iw') As "Opened Y-W",
-to_char(BG_DETECTION_DATE, 'iyyy') As "Opened Year",
-to_char(BG_DETECTION_DATE, 'mm') As "Opened Month",
-to_char(BG_DETECTION_DATE, 'iw') As "Opened Week",
+'to_char(BG_DETECTION_DATE, 'iyyy') As "Opened Year",
+'to_char(BG_DETECTION_DATE, 'mm') As "Opened Month",
+'to_char(BG_DETECTION_DATE, 'iw') As "Opened Week",
 to_char(BG_DETECTION_DATE, 'yyyy/mm/dd') As "Opened Date",
 ------------------------------------------------------------
 to_char(BG_CLOSING_DATE, 'iyyy')||'-'||to_char(BG_CLOSING_DATE, 'iw') As "CLosed Y-W",
-to_char(BG_CLOSING_DATE, 'iyyy') As "Closed Year",
-to_char(BG_CLOSING_DATE, 'mm') As "Closed Month",
-to_char(BG_CLOSING_DATE, 'iw') As "Closed Week",
+'to_char(BG_CLOSING_DATE, 'iyyy') As "Closed Year",
+'to_char(BG_CLOSING_DATE, 'mm') As "Closed Month",
+'to_char(BG_CLOSING_DATE, 'iw') As "Closed Week",
 to_char(BG_CLOSING_DATE, 'yyyy/mm/dd') As "Closed Date",
 ------------------------------------------------------------
 BG_STATUS As "Etat",
